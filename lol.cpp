0aea66b4a192025f7e9e88d67d5593f9cb7fa211
ahali00 <ali.com.com999@gmail.com>
2022-06-28 09:21:03 +0200

    i will add 2 files 1 to empety and 2 to c++ file

---
+++ lol.cpp
@@ -0,0 +1,132 @@
+/******************************************************************************
+
+Run On Terminal
+/*$ g++ -o hello hello.cc 
+$ ./hello 
+Hello, World!
+
+
+Online Compiler and debugger for c/c++
+Link Website :
+https://www.onlinegdb.com/
+
+*/
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+// Write C++ code here
+//============================================================================
+//#New Line
+   //M1
+   cout << "Hello Aloosh!"<<endl;
+   cout << "Fuck Sudan";
+   
+   //M2
+   cout << "Hello Aloosh! \n";
+   cout << "Fuck Sudan \n";
+   cout << "Hey\n Sudan";
+//============================================================================
+ 
+//#Variables
+   int MyVar_iable100 = 10; 
+   cout <<MyVar_iable100;
+   
+//int a, b;
+// defines two variables of type int
+
+int a = 30; 
+
+int b = 15; 
+
+int sum = a + b;
+cout << sum;
+
+//====================================================================
+
+int a;
+ cout << "Enter ur Fuckin Numbers \n";
+ cin >> a;
+ 
+ 
+//============================================================================
+
+int a;
+
+a = 10;
+cout << a;
+
+//============================================================================
+auto x = 4; //integer
+
+auto y = 3.37; //float
+
+auto z = "hello"; //string 
+//============================================================================
+int x = 20 - 10 * 5 + 3 - 10;
+cout << x; 
+
+//============================================================================
+
+//ex 1
+int x = 10;
+x += 4; // equivalent to x = x + 4
+x -= 5; // equivalent to x = x - 5
+cout << x;
+
+
+//ex 2
+x *= 3; // equivalent to x = x * 3
+x /= 2; // equivalent to x = x / 2
+x %= 4; // equivalent to x = x % 4
+
+
+//ex 3
+x++; //equivalent to x = x + 1
+
+
+//ex 4
+int x = 20;
+x++;
+cout << x;
+
+//ex 5
+int x = 5;
+cnt y = ++x;
+cout << y;
+// x is 6, y is 6
+
+
+//ex 6
+int x = 5;
+int y = x++;
+cout << y;
+// x is 6, y is 5
+
+//============================================================================
+
+//#Project Code
+int passengers;
+cout << "";
+cin >> passengers ;
+
+int left_passengers= passengers % 50;
+
+if(left_passengers == 0){
+    
+    cout << "0 seats left";
+}
+else{
+ int left_seats= 50 - left_passengers;
+    
+    cout << left_seats;
+    cout << "";
+}
+//============================================================================
+*/
+    return 0;
+}
